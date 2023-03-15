35d3ad9cf7e471cec709ba05822428c91f9ac30d
Cody Hulse <hulsecody@yahoo.com>
2023-03-15 12:54:23 -0700

    Added two custom rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,8 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
+    "arrow-body-style": ["error", "as-needed"],
+    "no-restricted-imports": ["error", { "name": "moment", "message": "Don't use moment unless absolutely necessary. Instead use date-fns when possible." }],
     "no-unused-vars": "off",
     "no-console": "off",
     "no-plusplus": "off",
